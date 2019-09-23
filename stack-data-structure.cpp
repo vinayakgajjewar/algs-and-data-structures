1a30d6b771634ade8b84cff013d1d0b895532af7
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-09-23 14:42:57 -0700

    Take notes on stacks

---
+++ stack-data-structure.cpp
@@ -0,0 +1,11 @@
+// A stack is a common data structure that represents things that need to be maintained in a specific order
+// When a function calls another function, which then calls a third function,
+// it is important that the third function returns to the second function rather than the first
+// You can think about it like the functions being stacked on top of each other
+// The last one added to the stack is the first one taken off
+// The "call stack" is the term for the list of functions either executing or waiting for other functions to finish
+// Push: To add an element onto the stack
+// Pop: To remove an element from the stack
+// Peek: To look at elements in the stack without removing them
+// LIFO: Last-in, first-out
+// FILO: Equivalent to LIFO, first-in, last-out
