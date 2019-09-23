3db9a470166d926b50df176b5dc6aeafd4c7c9c8
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-09-23 15:16:22 -0700

    Add code for stacks

--- stack-data-structure.cpp
+++ stack-data-structure.cpp
@@ -9,3 +9,57 @@
 // Peek: To look at elements in the stack without removing them
 // LIFO: Last-in, first-out
 // FILO: Equivalent to LIFO, first-in, last-out
+
+#include <iostream>
+
+class Stack {
+public:
+    Stack(int);
+    Stack();
+    void push(int);
+    int pop();
+    int peek(int);
+    void delete_stack();
+protected:
+    int *data;
+    int curr_num_elems;
+    int max_num_elems;
+};
+
+Stack::Stack(int max) {
+    max_num_elems = max;
+    data = new int[max_num_elems];
+    curr_num_elems = 0;
+}
+
+Stack::Stack() {
+    max_num_elems = 500;
+    data = new int[max_num_elems];
+    curr_num_elems = 0;
+}
+
+void Stack::push(int push_num) {
+    data[curr_num_elems] = push_num;
+    curr_num_elems++;
+}
+
+int Stack::pop() {
+    curr_num_elems--;
+    return data[curr_num_elems];
+}
+
+int Stack::peek(int depth) {
+    return data[curr_num_elems - (depth + 1)];
+}
+
+void Stack::delete_stack() {
+    delete[] data;
+}
+
+int main() {
+    Stack my_stack;
+    my_stack.push(15);
+    my_stack.push(10);
+    std::cout << my_stack.pop() << "\n";
+    std::cout << my_stack.pop() << "\n";
+}
