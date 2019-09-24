c00153d726fdca7eb0bb2141e4283e2ef154bb7f
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-09-24 15:49:09 -0700

    Add notes about queues

---
+++ queue-data-structure.cpp
@@ -0,0 +1,5 @@
+// Queues, like stacks, have restrictions on where you can add and remove elements
+// Queues are like a cafeteria line: the person in the front is served first and people are added to the back of the line
+// FIFO: First-in, first-out
+// Enqueue: the addition of an element to a queue
+// Dequeue: removing an element from a queue
